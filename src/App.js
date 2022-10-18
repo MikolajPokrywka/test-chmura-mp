ae2531bb91f56f62f841eedcc138bd22bd3710a7
Mikołaj Pokrywka <mp7961@gmail.com>
2022-10-18 20:54:56 +0200

    basic app

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my first App on Heroku :) <br/><br/>
+          Creator: Mikołaj Pokrywka 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
