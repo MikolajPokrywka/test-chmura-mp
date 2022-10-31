23f0b01ba81a7c90cada14cbbeec69a4a8b38648
Mikołaj Pokrywka <mp7961@gmail.com>
2022-10-31 11:02:02 +0100

    zaj_1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import  React, { useState , useEffect } from 'react'
 
 function App() {
+
+
+  var [date,setDate] = useState(new Date());
+    
+  useEffect(() => {
+      var timer = setInterval(()=>setDate(new Date()), 100 )
+      return function cleanup() {
+          clearInterval(timer)
+      }
+  
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is my first App on Heroku :) <br/><br/>
-          Creator: Mikołaj Pokrywka 
-        </p>
-      </header>
-    </div>
+    <>
+      <head>
+          <title>Rozwiązanie zadania PZC 1.1</title>
+      </head>
+      <body>
+      {date.toISOString()}
+      </body>
+      </>
   );
 }
 
